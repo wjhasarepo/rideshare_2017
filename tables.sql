96df6d00ec7d73214976c23154b3978a53a968c6
Wayne Jiao <wayne.jiao@keybridgeti.net>
2017-04-05 15:38:13 -0500

    database table schema

---
+++ tables.sql
@@ -0,0 +1,180 @@
+create table airport_positions
+{
+    airport_id integer not null primary key,
+    airport_name varchar(255),
+    airport_lat decimal,
+    airport_lng decimal,
+    created_at datetime not null,
+    updated_at datetime not null
+};
+
+
+
+create table device_users
+{
+    user_id varchar(255) not null auto_increment primary key,
+	first_name varchar(255),
+    last_name varchar(255),
+    phone_number varchar(255),
+    email varchar(255),
+    encrypted_password varchar(255) default ‘’,
+    sign_in_count integer default 0,
+    current_sign_in_at datetime,
+    last_sign_in_at datetime,
+    current_sign_in_ip varchar(255),
+    last_sign_in_ip varchar(255),
+    failed_attempts integer not null default 0,
+    unlock_token varchar(255),
+    locked_at datetime,
+    device_id varchar(255),
+    tokens text,
+    created_at datetime,
+    updated_at datetime
+};
+
+add_index device_users, [device_id], name: index_device_users_on_device_id
+add_index device_users, [email], name: index_device_users_on_email
+add_index device_users, [uid, provider], name: index_device_users_on_uid_and_provider, unique: true
+
+
+
+create table cars
+{ 
+    user_id integer not null primary key,
+    car_id integer not null auto_increment primary key,
+    year integer,  
+    make varchar(255),
+    model varchar(255),   
+    color varchar(255),
+    created_at datetime,
+    updated_at datetime,
+	foreign key (user_id) references device_users(user_id) on delete cascade
+};
+  
+add_index cars, [user_id], name: index_cars_on_user_id
+
+
+
+create table users_roles
+{ 
+    user_id integer not null primary key,  
+    role_id integer not null primary key,
+	type integer not null,
+    created_at datetime, 
+    updated_at datetime,
+	foreign key (user_id) references device_users(user_id) on delete cascade,
+	foreign key (resource_id) references cars(car_id) on delete cascade
+}
+
+add_index device_users_roles, [user_id, role_id], name: index_device_users_roles_on_user_id_and_role_id
+add_index roles, [name, resource_type, resource_id], name: index_roles_on_name_and_resource_type_and_resource_id
+add_index roles, [name], name: index_roles_on_name
+
+
+
+create table users_positions
+{
+	user_id integer not null primary key,
+    latitude decimal,   
+    longitude decimal,
+    created_at datetime not null,
+    updated_at datetime not null,
+	foreign key (user_id) references device_users(user_id) on delete cascade
+}
+
+add_index positions, [user_id], name: index_positions_on_user_id
+
+
+
+create table rides_offered
+{
+    rider_offer_id integer not null primary key,
+	user_id integer not null primary key,  
+	airport_id integer not null,
+    car_id integer,  
+	timing datetime, 
+    start varchar(255),   
+    start_lat  decimal,
+    start_lng decimal,  
+    destination varchar(255),   
+    destination_lat decimal,  
+    destination_lng decimal,  
+    available_passengers integer,  
+    available_bags integer,  
+    flex_value decimal,
+    flex_unit varchar(255),
+    created_at datetime not null,
+    updated_at datetime not null,
+	foreign key (user_id) references device_users(user_id) on delete cascade
+	foreign key (airport_id) references airport(airport_id) on delete cascade
+}
+  
+add_index rides_offered, [car_id], name: index_rides_offered_on_car_id
+  
+
+
+create table rides_requested
+{
+    ride_request_id integer not null primary key,
+    user_id integer not null primary key,
+	airport_id integer,
+    passengers integer,
+    bag_capacity integer,
+    timing datetime,
+    start_address varchar(255),
+    start_lat decimal,
+    start_lng decimal,
+    destination_address varchar(255),
+    destination_lat decimal,
+    destination_lng decimal,
+    created_at datetime not null,
+    updated_at datetime not null,
+	foreign key (user_id) references device_users(user_id) on delete cascade
+	foreign key (airport_id) references airport(airport_id) on delete cascade
+}
+
+
+
+create table rides_matches
+{ 
+	ride_match_id integer not null primary key,
+    ride_offer_id integer not null primary key, 
+    ride_request_id integer not null primary key,  
+    created_at datetime not null,
+    updated_at datetime not null,
+	foreign key (ride_offered_id) references rides_offered(ride_offer_id) on delete cascade
+	foreign key (ride_requested_id) references device_users(ride_request_id) on delete cascade
+}
+  
+add_index ride_matches, [ride_offered_id], name: index_ride_matches_on_ride_offered_id
+add_index ride_matches, [ride_requested_id], name: index_ride_matches_on_ride_requested_id
+
+
+
+create table rides_transactions
+{ 
+	ride_transaction_id integer not null primary key,
+    ride_match_id integer not null primary key, 
+    status varchar(255),
+    created_at datetime not null,
+    updated_at datetime not null,
+	foreign key (ride_match_id) references ride_matches(ride_match_id) on delete cascade
+}
+  
+add_index ride_transactions, [ride_match_id], name: index_ride_transactions_on_ride_match_id
+  
+
+
+create table transaction_reviews
+{
+    ride_transaction_id integer not null primary key, 
+	passenger_id integer not null primary key,
+	driver_id integer not null primary key,
+    rating integer,    
+    comment text,
+    created_at datetime not null,
+    updated_at datetime not null
+	foreign key (ride_match_id) references ride_matches(ride_transaction_id) on delete cascade
+}
+  
+add_index transaction_reviews, [ride_transaction_id], name: index_transaction_reviews_on_ride_transaction_id
