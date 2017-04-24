9c24ad162660aecbddffac2f9fc0dea9546fa2bf
Wayne Jiao <wayne.jiao@keybridgeti.net>
2017-04-24 17:48:15 -0500

    passport testing

--- server.js
+++ server.js
@@ -1,30 +1,51 @@
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
 var bodyParser = require("body-parser");
+var passport = require('passport');
 
 var customer = require('./routes/customer');
 var index = require('./routes/index');
 
 var app = express();
 
+require('./lib/passport')(passport);
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(session({
+	secret: 'vidyapathaisalwaysrunning',
+	resave: true,
+	saveUninitialized: true
+ } )); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', index.index);
 app.get('/customer', customer.index);
 app.get('/customer/:id', customer.show);
-// app.get('/customer', customer.show);
 app.post('/customer', customer.create);
-// app.get('/customers/add', customers.create);
-// app.post('/customers/add', customers.save);
 app.delete('/customer/delete/:id', customer.destroy)
-// app.get('/customers/delete/:id', customers.delete_customer);
-//app.get('/customers/edit/:id', customers.edit);
 app.put('/customer/update/:id',customer.update);
+
+app.post('/signup', passport.authenticate('local-signup',
+  function(req, res) {
+    console.log("hello");
+  })
+);
+app.post('/login', passport.authenticate('local-signin'));
+app.get('/logout', function(req, res){
+  var name = req.user.username;
+  console.log("LOGGIN OUT " + req.user.username)
+  req.logout();
+  res.redirect('/');
+  req.session.notice = "You have successfully been logged out " + name + "!";
+});
+
+
 
 /*
 var pool = mysql.createPool({
